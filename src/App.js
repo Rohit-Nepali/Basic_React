355a7ba02285ef3915b63b5a3817f713c0f8e107
Rohit Nepali <rohitnepali75@gmail.com>
2024-08-05 17:11:02 +0545

    using states, props and few more components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 //import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert.js';
 import Navbar from './components/Navbar.js'
 import TextForms from './components/TextForms.js'
 
 
 function App() {
+
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message,type) =>{
+    setAlert({
+      msg: message,
+      type:type
+    })
+  }
   return ( // this fucntion can return only one tag(container tag) <></> this is shorthand of reactFragment used to return mulitple elements in a single container 
     <>
       <Navbar title= "Main Text Utils"/>
+      <Alert alert={alert}/>
       <TextForms heading= "Enter text   "/>
     </>
   );
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -2,19 +2,42 @@ import React, { useState } from 'react'
 
 
 export default function TextForms(props) {
+    
+    const [text, setText] = useState("Enter text here");
+    const [rotationAngle,setRotationAngle] = useState(0);
+
     const handleUpClick = () => {
         //console.log("To uppercase ")
         let newText = text.toUpperCase();
         setText(newText)
         console.log(newText)
     }
+    const handleLoClick = () => {
+        //console.log("To uppercase ")
+        let newText = text.toLowerCase();
+        setText(newText)
+        console.log(newText)
+    }
+
+    const handleOnRotateClick =() =>{
+        setRotationAngle(rotationAngle + 45)
+    }
+
+
+    const handleOnClearClick = () =>{
+        setText("")
+    }
 
     const handlOnChange = (event) => {
         setText(event.target.value)
     }
 
-    const [text, setText] = useState("Enter text here");
-
+    const rotatedTextStyle = {
+        transform: `rotate(${rotationAngle}deg)`,
+        display: 'inline-block'
+    };
+ 
+    console.log(text.split(" ").length ); // split returns an empty array with 1 as the length.
     return (
         <>
             <div className='container'>
@@ -22,11 +45,16 @@ export default function TextForms(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handlOnChange} id="TextBox" rows="5"></textarea>
                 </div>
-                <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary ' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary ms-2 ' onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className='btn btn-primary ms-2 ' onClick={handleOnClearClick}>Clear Text</button>
+                <button className='btn btn-primary ms-2 my -2' onClick={handleOnRotateClick}>Rotate</button>
             </div>
             <div className='container my-3' >
                 <h1>Your text Summary</h1>
-                <p>{text.split(" ").length} words and {text.length} characters.</p>
+                <p>{text.split(" ").length} words, {text.length} characters and {text.split(".").length -1} sentences.</p>
+                <h2>Your text :</h2>
+                <p style={rotatedTextStyle}>{text}</p>
             </div>
         </>
     )
